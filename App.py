64fbba8ff470aec9e41dc9fe9ee9455596442db8
Mhijazi16 <gultimate0@gmail.com>
2024-06-27 23:18:02 +0300

    adding subheader

--- App.py
+++ App.py
@@ -4,5 +4,9 @@ def main():
     st.set_page_config(page_icon=":genie:",page_title="WebGenie")
     st.title("WebGenie :genie:")
 
+    with st.sidebar: 
+        st.subheader(":gear: Settings")
+        url = st.text_input(":link: Enter URL :")
+
 if __name__ == "__main__": 
     main()
