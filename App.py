f9fa6dadc852be1082c5fab40b2259317af2aa1e
Mhijazi16 <gultimate0@gmail.com>
2024-06-27 23:14:18 +0300

    adding title and icon to webpage

--- App.py
+++ App.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 def main():
-    pass
+    st.set_page_config(page_icon=":genie:",page_title="WebGenie")
+    st.title("WebGenie :genie:")
 
 if __name__ == "__main__": 
     main()
