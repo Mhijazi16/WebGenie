4d49ec70a42b276a505cc0da42e41cceaeea9980
Mhijazi16 <gultimate0@gmail.com>
2024-07-07 19:03:36 +0300

    adding some features

--- App.py
+++ App.py
@@ -1,15 +1,15 @@
+from pydantic.v1.schema import schema
 import streamlit as st
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain_community.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-import ollama
+from langchain_community.llms import Ollama 
 from langchain.vectorstores import FAISS
+from langchain_community.embeddings import OllamaEmbeddings 
+from langchain_core.prompts import PromptTemplate
+from langchain.schema.output_parser import StrOutputParser
 
-class OllamaEmbeddingWrapper:
-    def embed_documents(self, texts):
-        return [ollama.embeddings(model="nomic-embed-text", prompt=text)['embedding'] for text in texts]
-    def __call__(self, text):
-        return ollama.embeddings(model="nomic-embed-text", prompt=text)['embedding']
+llm = Ollama(model="llama3")
 
 def get_retriever(url): 
     # scraping the url to text
@@ -21,13 +21,26 @@ def get_retriever(url):
     chunks = splitter.split_documents(document)
 
     #creating vectorstore 
-    store = FAISS.from_texts(chunks,embedding=OllamaEmbeddingWrapper())
-
+    store = FAISS.from_documents(chunks,
+                             embedding=OllamaEmbeddings(model="nomic-embed-text"))
     # returning a retriever 
     return store.as_retriever() 
 
-def get_response(prompt):
-    return "Yes I agree"
+def get_response(prompt,retriever):
+
+    with st.spinner("retriever in progress"):
+        info = retriever.invoke(prompt)
+
+    template = f"""
+        You are the WebGenie an AI assistant that scrapes URL then answers 
+        questions based on the context you have.
+        You need to use Markdown and emoji's when you output results
+        here is the Context : {info}
+        here is the question : {prompt}
+    """
+
+    with st.spinner("genereating response"):
+        return llm.stream(template)
 
 def main():
     st.set_page_config(page_icon=":genie:",page_title="WebGenie")
@@ -35,8 +48,7 @@ def main():
 
     if "chat_history" not in st.session_state: 
         st.session_state.chat_history = [
-            HumanMessage("Hello what are you ?"),
-            AIMessage("Hello Am the WebGenie how can I help you!!"),
+            HumanMessage("Explain What you do ?"),
         ]
 
     with st.sidebar: 
@@ -45,23 +57,27 @@ def main():
 
     if url == None or url == "": 
         st.info(":warning: Please provide a URL to Enable chatting.")
+
+    if "retriever" not in st.session_state: 
+        st.session_state.retriever = None
     else : 
-        docs = get_vectorstore(url)
+        if st.session_state.retriever == None :
+            st.session_state.retriever = get_retriever(url) 
+
         prompt = st.chat_input("type your prompt here...")
+
         if prompt != None and prompt != "": 
             st.session_state.chat_history.append(HumanMessage(prompt))
-            st.session_state.chat_history.append(AIMessage(get_response(prompt,url)))
+
 
         for message in st.session_state.chat_history: 
             if isinstance(message,AIMessage): 
                 with st.chat_message("AI"): 
-                    st.write(message.content)
+                    res = st.write(get_response(prompt,st.session_state.retriever))
             elif isinstance(message,HumanMessage): 
                 with st.chat_message("Human"): 
                     st.write(message.content)
 
-        with st.sidebar: 
-            st.write(docs)
 
 if __name__ == "__main__": 
     main()
